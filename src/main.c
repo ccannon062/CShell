48ce2b61b5969d95708d5e859644b9d7e875fccc
Caleb C <118626040+ccannon062@users.noreply.github.com>
2025-11-07 13:03:37 -0600

    Exit builtin

--- src/main.c
+++ src/main.c
@@ -2,13 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+void slice(char *src, char *dest) {
+    int j = 0;
+    for (int i = 0; i < 4; i++) {
+        dest[j++] = src[i];
+    }
+    dest[j] = '\0';
+}
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   char input[100];
+  char result[100];
   while(1) {
     printf("$ ");
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    slice(input, result);
+    if (strcmp(result, "exit") == 0) {
+        if(input[5] == '1') {
+          return 1;
+        }
+        break;
+    }
     printf("%s: command not found\n", input);
   }
   return 0;
