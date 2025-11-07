57a12b183b367ac39bc8b9bc88b17420a2e84a66
Caleb C <118626040+ccannon062@users.noreply.github.com>
2025-11-07 12:14:54 -0600

    Command not found

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
+  char input[100];
   printf("$ ");
-
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
+  fgets(input, 100, stdin);
   return 0;
 }
