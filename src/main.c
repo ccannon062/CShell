5b72bf1122e588844ab3b3e06289afe9c464540c
Caleb C <118626040+ccannon062@users.noreply.github.com>
2025-11-07 20:24:44 -0600

    Echo functionality

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void slice(char *src, char *dest) {
+void parseString(int start, int end, char *src, char *dest) {
     int j = 0;
-    for (int i = 0; i < 4; i++) {
+    for (int i = start; i < end; i++) {
         dest[j++] = src[i];
     }
     dest[j] = '\0';
@@ -18,14 +18,19 @@ int main(int argc, char *argv[]) {
     printf("$ ");
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-    slice(input, result);
-    if (strcmp(result, "exit") == 0) {
-        if(input[5] == '1') {
+    parseString(0, 5, input, result);
+    if (strcmp(result, "exit ") == 0) {
+        if(input[5] == '1' && strlen(input) == 6) {
           return 1;
+        } else if (input[5] == '0' && strlen(input) == 6) {
+          return 0;
         }
-        break;
+    } else if(strcmp(result, "echo ") == 0) {
+      parseString(5, strlen(input), input, result);
+      printf("%s\n", result);
+    } else {
+       printf("%s: command not found\n", input);
     }
-    printf("%s: command not found\n", input);
   }
   return 0;
 }
